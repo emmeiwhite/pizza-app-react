6c1624b0bc6d0c335f8eb57288b627a6a3a9ae88
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-03-25 00:34:32 +0530

    Getting started with the Pizza app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.querySelector("#root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Pizza = () => {
+  return (
+    <div>
+      <img
+        src="pizzas/focaccia.jpg"
+        alt="pizza"
+      />
+      <h2>Focaccia Pizza</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <section>
+      <Header />
+      <Menu />
+      <Footer />
+    </section>
+  );
+};
+
+// Let's create components to be used in the application
+function Menu() {
+  return (
+    <div>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Footer() {
+  return (
+    <p>
+      {" "}
+      &copy; Pizza Palace {new Date().toLocaleTimeString()}! We are currently
+      open
+    </p>
+  );
+}
+
+const root = ReactDOM.createRoot(el);
+root.render(<App />);
